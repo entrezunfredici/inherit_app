92846e34e98f77daf47b5ded39731c23f0675b9c
Yoan <mrts.yoan@gmail.com>
2025-03-07 10:13:23 +0100

    guerrier

---
+++ guerrier.py
@@ -0,0 +1,15 @@
+from personnage import Personnage
+
+class Guerrier(Personnage):
+    def __init__(self, nom, pv, force, intelligence, endurance, mana):
+        super().__init__(nom, pv, force, intelligence, endurance, mana)
+
+    def frapper(self):
+        if self.endurance > 0:
+            degats = self.endurance * 2
+            self.endurance -= 1
+            print(f"{self.nom} frappe avec une force de {degats} points de dégâts!")
+            return degats
+        else:
+            print(f"{self.nom} n'a plus d'endurance pour frapper.")
+            return 0
