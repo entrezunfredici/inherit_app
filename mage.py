49cb05e7a8b8d9db80f26c5618e965b410503734
entrezunfredici <superfred2468@gmail.com>
2025-03-07 10:11:39 +0100

    [ADD] attaque

--- mage.py
+++ mage.py
@@ -5,6 +5,8 @@ def Mage(Personnage):
     Personnage.intelligence = 50
     Personnage.force = 150
     Personnage.name = "kévin"
+    mana = 100
 
-    
-
+    def attaque(self):
+        super().attaquer()
+        self.mana = self.mana - 10
